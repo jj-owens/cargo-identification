b3c50d7059b9a25d27c605ace1e1b589b274ecb3
jjowens <jj.owens.dev@gmail.com>
2017-10-18 12:17:35 -0400

    Refactor optimization Refactor string split to single var Remove obsolete if/else expressions Move var declarations Fix uppercase logic Add arrow functions:

--- cargo.js
+++ cargo.js
@@ -1,59 +1,47 @@
 function cargoNumberValidate(string) {
+    const data = string;
     if (typeof(string) != "string"){
         console.log("Input must be a string!")
         return "Input must be a string!";
-    } else {
-        string.toUpperCase();
     }
     
     const registration_number_validate = /^[0-9]+$/;
     const product_group_validate = /[J|U|Z]/;
     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const values = [10,12,13,14,15,16,17,18,19,20,21,23,24,25,26,27,28,29,30,31,32,34,35,36,37,38]
-    var owner_code = string.split('').slice(1, 4);
-    var product_group_code = string.split('').slice(3,4);
-    var registration_number = string.split('').slice(4, 10);
-    var check_digit = string.split('').slice(-1).join('');
-    var aprefix = string.split('').slice(0, 4);
-    var check_calculated = registrationCalc(string); 
+    const stringarr = data.toUpperCase().split('');
+    var owner_code = stringarr.slice(1, 4);
+    var product_group_code = stringarr.slice(3,4);
+    var registration_number = stringarr.slice(4, 10);
+    var check_digit = stringarr.slice(-1).join('');
+    var aprefix = stringarr.slice(0, 4);
+    var check_calculated = registrationCalc(data); 
   
-    if (string.length !== 11){
+    if (data.length !== 11) {
         return "Invalid string length, please check the container id and try again.";
-    } else {
-        //return 'Container id is correct length..'; 
-    }
-    if (owner_code.join('') == /[A-Z]/) {
-        //return 'Owner code must have all capital letters. Please check container id and try again..';
-    } else {
-        //return 'Owner code in correct format..';
-    }
+    } 
     if (product_group_validate.test(product_group_code.join('')) === false){
-        //return "Product group code must be a J, U, or Z..";
-    } else {
-        //return 'Product group code in correct format..';
+        return "Product group code must be a J, U, or Z..";
     }
-    if (registration_number_validate.test(registration_number.join('')) === true && registration_number.length === 6){
-       //return 'Registration number in the correct format..';
-    } else {
+    if (registration_number_validate.test(registration_number.join('')) !== true || registration_number.length !== 6){
         return 'Registration number must be all numbers and be 6 digits long. Zeros may proceed a number to make it 6 digits in length..';
     }
 
-
-    function registrationCalc(string) {
+    function registrationCalc(data) {
         const powers = [1,2,4,8,16,32,64,128,256,512];
-        let alpha_calc = aprefix.map(function(element, index){
+        let alpha_calc = aprefix.map((element, index) => {
             return values[alphabet.indexOf(element)] * powers[index];
         });
  
-        let digit_calc = registration_number.map(function(element, index){
+        let digit_calc = registration_number.map((element, index) => {
             return element * powers[index + 4];
         });
 
-        let x = alpha_calc.reduce(function(a, b){
+        let x = alpha_calc.reduce((a, b) => {
             return a + b;
         });
 
-        let y = digit_calc.reduce(function(a, b){
+        let y = digit_calc.reduce((a, b) => {
             return a + b;
         }); 
 
@@ -69,4 +57,4 @@ function cargoNumberValidate(string) {
     }
 }
 
-cargoNumberValidate("SUDU3070079");
+cargoNumberValidate("SuDU3070079");
